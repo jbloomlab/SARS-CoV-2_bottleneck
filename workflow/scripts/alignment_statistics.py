0d9049459037060fa52073d7f466f6118569893d
Will Hannon <wwh22@uw.edu>
2021-01-06 15:22:24 -0800

    Started working on a module to collect information on alignment stats.

---
+++ workflow/scripts/alignment_statistics.py
@@ -0,0 +1,11 @@
+"""
+This module contains utilities for assessing short-read 
+alignments. Most utilities are wrappers around various 
+`pysam` functions. 
+"""
+__author__ = "Will Hannon"
+__copyright__ = "Copyright 2021 Will Hannon"
+__email__ = "wwh22@uw.edu"
+__license__ = "MIT"
+
+import pysam
